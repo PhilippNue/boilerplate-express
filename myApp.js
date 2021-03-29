178bc0a5243fd4b6d34b0e793ee4c668cc95792a
Nuephil <philipp.nuetzi@outlook.com>
2021-03-29 23:58:18 +0200

    implemented node.js

--- myApp.js
+++ myApp.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.use("/public", express.static(__dirname + "/public"));
+
 var absolutePath = __dirname + '/views/index.html';
 app.get("/",function(req, res) {
               res.sendFile(absolutePath);
             });
 
 
-
-console.log("Hello World");
 
 
 
