dae1e6003231a8183129873961c1c02ce58d40f4
Nuephil <philipp.nuetzi@outlook.com>
2021-03-30 20:51:19 +0200

    added get (/json)

--- myApp.js
+++ myApp.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
-
 app.use("/public", express.static(__dirname + "/public"));
 
 var absolutePath = __dirname + '/views/index.html';
@@ -10,6 +9,7 @@ app.get("/",function(req, res) {
               res.sendFile(absolutePath);
             });
 
+app.get("/json", function(req, res){res.json({"message": "Hello json"})});
 
 
 
