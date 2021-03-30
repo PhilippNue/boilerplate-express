714e97da6ea012072a9469a612f2220d8b2cb2bf
Nuephil <philipp.nuetzi@outlook.com>
2021-03-30 20:59:21 +0200

    added get .env

--- myApp.js
+++ myApp.js
@@ -9,7 +9,16 @@ app.get("/",function(req, res) {
               res.sendFile(absolutePath);
             });
 
-app.get("/json", function(req, res){res.json({"message": "Hello json"})});
+app.get("/json", function(req, res){
+
+if(process.env.MESSAGE_STYLE = "uppercase")
+{
+res.json({"message": "HELLO JSON"})
+}
+else{
+res.json({"message": "Hello json"});
+}
+});
 
 
 
